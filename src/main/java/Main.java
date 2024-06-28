cb20426327baed766692cfffbe82a960409590d7
Anastasiya-Vavilova <ntttru@bk.ru>
2024-06-28 17:17:55 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "5555 № 555558";
+        post.patronymic = "Иванович";
+        post.phone = "+7(999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+
+
+
+    }
+
+
+
+
+}
